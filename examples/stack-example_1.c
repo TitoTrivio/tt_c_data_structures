5cdee5c5960acb0159d203e2e26b14fb23664bba
TitoTrivio <titotrivio@gmail.com>
2026-03-25 20:58:18 -0300

    feat: stack example added

---
+++ examples/stack-example_1.c
@@ -0,0 +1,32 @@
+#include "stack.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+    Stack stack;
+    stack_init(&stack, 10);
+
+    int *number = (int*) malloc(sizeof(int));
+    char *text = (char*) malloc(12 * sizeof(char));
+
+    *number = 3;
+    strcpy(text, "Hello World");
+
+    stack_push(&stack, number);
+    stack_push(&stack, text);
+
+    char *text2 = (char*) stack_pop(&stack);
+    int *number2 = (int*) stack_pop(&stack);
+
+    printf("%s\n", text2);
+    printf("%d\n", *number2);
+
+    stack_free(&stack);
+    free(text2);
+    free(number2);
+    
+    return 0;
+}
+
