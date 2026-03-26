9a4f43506d716a3c33718cb40ade25c05b898e7f
TitoTrivio <titotrivio@gmail.com>
2026-03-26 13:49:03 -0300

    feat: Type specific operations added to stack

--- include/stack.h
+++ include/stack.h
@@ -1,7 +1,7 @@
 #ifndef TT_STACK_H
 #define TT_STACK_H
 
-// Stack definition
+/* Stack definition */
 
 struct _Stack
 {
@@ -12,7 +12,7 @@ struct _Stack
 
 typedef struct _Stack Stack;
 
-// Stack functions
+/* Stack creation and destruction */
 
 int stack_initialize(Stack *stack, int size);
 
@@ -22,9 +22,33 @@ Stack *stack_new(int size);
 
 void stack_free(Stack *stack);
 
+/* Stack operations */
+
 int stack_push(Stack *stack, void *value);
 
 void *stack_pop(Stack *stack);
+
+/* Type specific operations */
+
+int stack_push_int(Stack *stack, int value);
+
+int stack_push_char(Stack *stack, char value);
+
+int stack_push_float(Stack *stack, float value);
+
+int stack_push_double(Stack *stack, double value);
+
+int stack_push_str(Stack *stack, char *value);
+
+int stack_pop_int(Stack *stack);
+
+char stack_pop_char(Stack *stack);
+
+float stack_pop_float(Stack *stack);
+
+double stack_pop_double(Stack *stack);
+
+char *stack_pop_str(Stack *stack);
 
 #endif
 
--- src/stack.c
+++ src/stack.c
@@ -82,3 +82,93 @@ void *stack_pop(Stack *stack)
     return value;
 }
 
+int stack_push_int(Stack *stack, int value)
+{
+    int *ptr = (int*) malloc(sizeof(int));
+    
+    *ptr = value;
+
+    return stack_push(stack, ptr);
+}
+
+int stack_push_char(Stack *stack, char value)
+{
+    char *ptr = (char*) malloc(sizeof(char));
+    
+    *ptr = value;
+
+    return stack_push(stack, ptr);
+}
+
+int stack_push_float(Stack *stack, float value)
+{
+    float *ptr = (float*) malloc(sizeof(float));
+    
+    *ptr = value;
+
+    return stack_push(stack, ptr);
+}
+
+int stack_push_double(Stack *stack, double value)
+{
+    double *ptr = (double*) malloc(sizeof(double));
+    
+    *ptr = value;
+
+    return stack_push(stack, ptr);
+}
+
+int stack_push_str(Stack *stack, char *value)
+{
+    return stack_push(stack, value);
+}
+
+int stack_pop_int(Stack *stack)
+{
+    int *ptr = stack_pop(stack);
+
+    int value = *ptr;
+
+    free(ptr);
+
+    return value;
+}
+
+char stack_pop_char(Stack *stack)
+{
+    char *ptr = stack_pop(stack);
+
+    char value = *ptr;
+
+    free(ptr);
+
+    return value;
+}
+
+float stack_pop_float(Stack *stack)
+{
+    float *ptr = stack_pop(stack);
+
+    float value = *ptr;
+
+    free(ptr);
+
+    return value;
+}
+
+double stack_pop_double(Stack *stack)
+{
+    double *ptr = stack_pop(stack);
+
+    double value = *ptr;
+
+    free(ptr);
+
+    return value;
+}
+
+char *stack_pop_str(Stack *stack)
+{
+    return (char*) stack_pop(stack);
+}
+
