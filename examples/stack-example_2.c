9dec4ff795c182fa65e9be2a791f368df4598685
TitoTrivio <titotrivio@gmail.com>
2026-04-03 16:35:00 -0300

    refactor: dinamically allocate the stacks in the second example

--- examples/stack-example_2.c
+++ examples/stack-example_2.c
@@ -3,29 +3,29 @@
 
 int main(void)
 {
-    Stack str_stack, int_stack;
+    Stack *str_stack, *int_stack;
 
-    stack_initialize(&str_stack, 3);
-    stack_initialize(&int_stack, 3);
+    str_stack = stack_new(3);
+    int_stack = stack_new(3);
 
-    stack_push_str(&str_stack, "Three\n");
-    stack_push_str(&str_stack, "Two\n");
-    stack_push_str(&str_stack, "One\n");
+    stack_push_str(str_stack, "Three\n");
+    stack_push_str(str_stack, "Two\n");
+    stack_push_str(str_stack, "One\n");
     
-    stack_push_int(&int_stack, 3);
-    stack_push_int(&int_stack, 2);
-    stack_push_int(&int_stack, 1);
+    stack_push_int(int_stack, 3);
+    stack_push_int(int_stack, 2);
+    stack_push_int(int_stack, 1);
 
-    printf(stack_pop_str(&str_stack));
-    printf(stack_pop_str(&str_stack));
-    printf(stack_pop_str(&str_stack));
+    printf(stack_pop_str(str_stack));
+    printf(stack_pop_str(str_stack));
+    printf(stack_pop_str(str_stack));
     
-    printf("%d\n", stack_pop_int(&int_stack));
-    printf("%d\n", stack_pop_int(&int_stack));
-    printf("%d\n", stack_pop_int(&int_stack));
+    printf("%d\n", stack_pop_int(int_stack));
+    printf("%d\n", stack_pop_int(int_stack));
+    printf("%d\n", stack_pop_int(int_stack));
 
-    stack_finalize(&str_stack, false);
-    stack_finalize(&int_stack, true);
+    stack_free(str_stack, false);
+    stack_free(int_stack, true);
     
     return 0;
 }
